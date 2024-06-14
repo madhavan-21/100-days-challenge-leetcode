8192a6bd7d2bc71833c217bad8ff9ac222e4cd57
madhavan-21 <madhavanmadhavan42501@gmail.com>
2024-06-14 07:59:53 +0530

    day 2 completed

--- day - 2 .js
+++ day - 2 .js
@@ -1,3 +1,7 @@
+//283. Move Zeroes
+//Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+//Note that you must do this in-place without making a copy of the array.
 var moveZeroes = function(nums) {
     let left = 0;
     for(let right = 0;right<nums.length;right++){
