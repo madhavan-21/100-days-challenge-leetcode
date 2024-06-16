4eefff58bbebad62228032ece6e84b05129ac7f2
madhavan-21 <madhavanmadhavan42501@gmail.com>
2024-06-16 09:25:41 +0530

    day-4

---
+++ day - 4 .js
@@ -0,0 +1,25 @@
+//leetcode - 31. Next Permutation
+//A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+
+var nextPermutation = function(nums) {
+    let n = nums.length;
+    let k ,l;
+    for(k=n-2;k>=0;k--){
+        if(nums[k]<nums[k+1]){
+          break;
+            
+        }
+    }
+    if(k<0){
+        nums.reverse();
+    }else{
+        for(l=n-1;l>k;l--){
+            if(nums[l]>nums[k]){
+                break;
+            }
+        }
+
+        [nums[k],nums[l]] = [nums[l], nums[k]];
+        nums.splice(k+1,n-k,...nums.slice(k+1).reverse());
+    }
+};
