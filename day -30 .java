c749e66ad3a3e5d7cb0da0abbdfb977d5b085115
madhavan-21 <madhavanmadhavan42501@gmail.com>
2024-07-12 08:32:51 +0530

    day - 30

---
+++ day -30 .java
@@ -0,0 +1,22 @@
+1. Two Sum
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int s=nums.length;
+        HashMap <Integer, Integer> mp=new HashMap<>();
+        for(int i =0;i<s;i++)
+        {   
+            int n=target-nums[i]; 
+            
+            if(mp.containsKey(n)) 
+            {
+                return new int[]{i,mp.get(n)};  
+            }
+            mp.put(nums[i],i); 
+             
+            
+           
+        }
+        return new int[0];
+}
+}
