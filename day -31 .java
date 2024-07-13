fa24750416c74edcaa6cb6f212ef037f83da7dc7
madhavan-21 <madhavanmadhavan42501@gmail.com>
2024-07-13 19:37:02 +0530

    day - 31

---
+++ day -31 .java
@@ -0,0 +1,20 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int i;
+        Set<Integer> set = new HashSet<>();
+        for(i=0;i<nums.length;i++){
+            set.add(nums[i]);
+
+        }
+        i=1;
+        while(i<=nums.length){
+            if(set.contains(i)){
+                i++;
+            }else{
+                return i;
+            }
+        }
+        return i;
+        
+    }
+}
