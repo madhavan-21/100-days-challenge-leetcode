b11cd6c3d42270f4d74f8cb2db4e69ee8a01d23a
madhavan-21 <madhavanmadhavan42501@gmail.com>
2024-07-15 08:25:06 +0530

    day - 35

---
+++ day -33 .java
@@ -0,0 +1,25 @@
+
+
+class Solution {
+    public int arithmeticTriplets(int[] nums, int diff) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num: nums) {
+            if (map.containsKey(num)) {
+			    // the current number forms an arithmetic sequence with some previous number
+				// increment the length of this sequence and update the starting point
+                map.put(num + diff, map.get(num) + 1);
+                map.remove(num);
+            } else {
+			    // start a new arithmetic sequence, storing the next number we expect to see
+                map.put(num + diff, 1);
+            }
+        }
+		
+		// count the total number of arithmetic tuples from all the arithmetic sequences found
+        int res = 0;
+        for (int key: map.keySet()) {
+            res += Math.max(0, map.get(key) - 3 + 1);
+        }
+        return res;
+    }
+}
