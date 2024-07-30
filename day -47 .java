d6224f424342fac119f738e537de8d223366c097
madhavan-21 <madhavanmadhavan42501@gmail.com>
2024-07-30 08:22:15 +0530

    day - 47

---
+++ day -47 .java
@@ -0,0 +1,31 @@
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if(head == null) return head;
+
+        ListNode temp = null;
+        ListNode curr = head;
+        ListNode pre = null;
+        int count = 0;
+        while(curr != null && count < k){
+            curr = curr.next;
+            count++;
+        }
+        curr = head;
+        if(count == k) {
+            count = 0;
+            while(curr != null && count < k){
+                temp = curr.next;
+                curr.next = pre;
+                pre = curr;
+                curr = temp;
+                count++;
+            }
+        }else {
+            pre = head;
+        }
+        if(temp != null){
+            head.next = reverseKGroup(temp, k);
+        }
+        return pre;
+    }
+}
